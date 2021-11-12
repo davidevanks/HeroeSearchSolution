573d571982acc5912841eefd0894e273b422faa6
Kenneth Gaitán <kenneth.gaitan-ext@dominion-global.com>
2021-11-12 14:35:55 -0600

    Task 2: Details Page Terminada. ajustes en _layout e implementación de backend para detalle de heroe

---
+++ HeroeSearchWeb/Controllers/HeroeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HeroeSearchWeb.Data.Interfaces;
+using HeroeSearchWeb.Data.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace HeroeSearchWeb.Controllers
+{
+    public class HeroeController : Controller
+    {
+        private readonly IDetailsRepository _Details;
+
+        public HeroeController(IDetailsRepository DetailsRepository)
+        {
+            _Details = DetailsRepository;
+        }
+        [HttpGet("character/{id}")]
+        public IActionResult Details(int id)
+        {
+
+            Task<Heroe> Response;
+            Response = _Details.HeroesDetails(id);
+            Response.Result.ValueSearch = HttpContext.Session.GetString("searchString");
+            return View(Response);
+        }
+    }
+}
