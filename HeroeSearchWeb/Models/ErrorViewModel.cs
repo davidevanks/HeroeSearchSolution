6326f3e99f615b9a5239a0b98498b8ecf7f55fc4
Kenneth Gaitán <kenneth.gaitan-ext@dominion-global.com>
2021-11-12 14:02:03 -0600

    Add project files.

---
+++ HeroeSearchWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeroeSearchWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
